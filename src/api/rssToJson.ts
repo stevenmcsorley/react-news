7ce23d18ffd37e36a6966af95798c8e8d9fd2c23
Steven McSorley <smcsorl@sky.com>
2020-10-17 10:35:33 +0100

    change news feed source for ticker

--- src/api/rssToJson.ts
+++ src/api/rssToJson.ts
@@ -4,7 +4,7 @@ import BaseApiRssToJson from "./baseApiRssToJson";
 
 export default {
   getBreakingNews(): Promise<AxiosResponse<any>> {
-    return BaseApiRssToJson.get(`api.json?rss_url=http://rss.cnn.com/rss/edition.rss&count=7&api_key=5ivfzdrkuqwmoe0dgxeqvhfz0knlo7yq4fw20bt0`);
+    return BaseApiRssToJson.get(`api.json?rss_url=http://feeds.bbci.co.uk/news/rss.xml`);
   },
 
 };
