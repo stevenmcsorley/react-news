b19f6e42a78169ad49003acd11a3f9d2f9b5b363
Steven McSorley <smcsorl@sky.com>
2020-10-30 03:33:01 +0000

    related section layout fix

--- src/components/cards/CardRelated.tsx
+++ src/components/cards/CardRelated.tsx
@@ -18,15 +18,13 @@ const CardRelated: FunctionComponent<CardProps> = ({
   const placeholderImage =
     "https://c.pxhere.com/photos/aa/fa/newspaper_news_information_read_press_daily_newspaper_paper_magazines-811573.jpg!d";
   return (
-    <div>
-      <div className="dev-card-base dev-flex-column dev-u-padding-default dev-u-margin-bottom">
-        <div className="dev-card-base__header dev-u-padding-default">
-          <h4 className="dev-u-padding-horizontal card-title">{title}</h4>
-        </div>
-        <div className="dev-card-base__body dev-card-base__body--grow dev-u-padding-default">
-          <div className="dev-card-base__image" onClick={onClick}>
-            <img src={image ? image : placeholderImage} alt="" />
-          </div>
+    <div className="dev-card-base dev-flex-column dev-u-padding-default dev-u-margin-bottom">
+      <div className="dev-card-base__header dev-u-padding-default">
+        <h4 className="dev-u-padding-horizontal card-title">{title}</h4>
+      </div>
+      <div className="dev-card-base__body dev-card-base__body--grow dev-u-padding-default">
+        <div className="dev-card-base__image" onClick={onClick}>
+          <img src={image ? image : placeholderImage} alt="" />
         </div>
       </div>
     </div>
